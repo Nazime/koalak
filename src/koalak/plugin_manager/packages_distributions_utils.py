1531a082b39404751014582f2ea6d330a8b85f69
Lakehal Nazime <nazime.lkh@gmail.com>
2025-03-16 23:31:30 +0100

    u

--- src/koalak/plugin_manager/packages_distributions_utils.py
+++ src/koalak/plugin_manager/packages_distributions_utils.py
@@ -1,8 +1,7 @@
+import importlib.metadata
 import os
 import types
 from importlib.metadata import PathDistribution
-import importlib.metadata
-
 
 __all__ = "module_to_distribution"
 
@@ -10,8 +9,10 @@ __all__ = "module_to_distribution"
 
 _map_modules_to_distribution = {}
 _seen_root_packages_names = set()
+
+
 def module_to_package_distribution_name(module: types.ModuleType | str) -> str:
-    """All this module is here for this function, """
+    """All this module is here for this function,"""
     # Declare global variables for caching
     global _map_modules_to_distribution
     global _seen_root_packages_names
@@ -23,7 +24,7 @@ def module_to_package_distribution_name(module: types.ModuleType | str) -> str:
         module_name = module
 
     # get root package
-    root_package_name = module_name.split('.')[0]
+    root_package_name = module_name.split(".")[0]
 
     # Check if we already parsed the root_package
     if root_package_name in _seen_root_packages_names:
@@ -42,14 +43,20 @@ def module_to_package_distribution_name(module: types.ModuleType | str) -> str:
     _seen_root_packages_names.add(root_package_name)
     return _map_modules_to_distribution[module_name]
 
-def get_modules_from_distribution(distribution: PathDistribution, root_package_name)-> list[str]:
+
+def get_modules_from_distribution(
+    distribution: PathDistribution, root_package_name
+) -> list[str]:
     # If distribution is installed in 'editable' mode, check the editable file
     #  ex: "venv/lib/python3.13/site-packages/__editable__.koalak-0.3.4.pth
     if is_editable_distribution(distribution):
-        editable_filename = [str(e) for e in distribution.files if str(e).startswith("__editable__")]
+        editable_filename = [
+            str(e) for e in distribution.files if str(e).startswith("__editable__")
+        ]
         if len(editable_filename) != 1:
             raise ValueError(
-                f"distribution.files for '{distribution.name}' expect to have one file starting with __editable__ not {len(editable_filename)}")
+                f"distribution.files for '{distribution.name}' expect to have one file starting with __editable__ not {len(editable_filename)}"
+            )
         editable_filename = editable_filename[0]
         editable_filepath = distribution.locate_file(editable_filename)
         with open(editable_filepath) as f:
@@ -60,7 +67,7 @@ def get_modules_from_distribution(distribution: PathDistribution, root_package_n
 
 
 def get_modules_from_root_package(directory: str) -> list[str]:
-    """Return all modules from a given filepath that represent a root dir """
+    """Return all modules from a given filepath that represent a root dir"""
     modules = []
     for root, dirs, files in os.walk(directory):
         for file in files:
@@ -69,13 +76,16 @@ def get_modules_from_root_package(directory: str) -> list[str]:
 
             # Create the module path by removing the .py extension
             module_path = os.path.relpath(os.path.join(root, file), directory)
-            module_name =_clean_modulename(module_path)
+            module_name = _clean_modulename(module_path)
 
             # Add the module to the list
             modules.append(module_name)
     return modules
 
-def get_modules_from_distribution_files(distribution: PathDistribution, root_package_name: str) -> list[str]:
+
+def get_modules_from_distribution_files(
+    distribution: PathDistribution, root_package_name: str
+) -> list[str]:
     modules = []
     for file in distribution.files:
         file = str(file)
@@ -96,12 +106,12 @@ def is_editable_distribution(dist):
             return True  # It's an editable install
     return False
 
+
 def _clean_modulename(module_name: str) -> str:
     module_name = module_name.removesuffix(".py")
-    module_name = module_name.replace('/', ".").replace('\\', '.')
+    module_name = module_name.replace("/", ".").replace("\\", ".")
     # Handle __init__.py as a package
     if module_name.endswith("__init__"):
         # If it's __init__.py, treat it as a package, not a module
         module_name = module_name.removesuffix(".__init__")
-    return  module_name
-
+    return module_name
--- tests/test_plugin_managers/test_utils/test_packages_distributions_utils.py
+++ tests/test_plugin_managers/test_utils/test_packages_distributions_utils.py
@@ -1,7 +1,12 @@
-from koalak.plugin_manager.packages_distributions_utils import module_to_package_distribution_name
+from koalak.plugin_manager.packages_distributions_utils import (
+    module_to_package_distribution_name,
+)
+
 
 def test_module_to_package_distribution_name():
     # Koalak is a package with only one distribution so test here is straitforward
-    assert module_to_package_distribution_name('koalak.plugin_manager.consts') == "koalak"
-    assert module_to_package_distribution_name('koalak.plugin_manager') == "koalak"
-    assert module_to_package_distribution_name('yaml') == "PyYAML"
+    assert (
+        module_to_package_distribution_name("koalak.plugin_manager.consts") == "koalak"
+    )
+    assert module_to_package_distribution_name("koalak.plugin_manager") == "koalak"
+    assert module_to_package_distribution_name("yaml") == "PyYAML"
